c4030d32c673ffab9d0b112a02d9601b6f0e3fa8
QianBuis <1972qian@gmail.com>
2026-02-14 15:59:56 +0100

    Database Connectie gemaakt - en Test toegevoegd om te controleren of hij wel echt connect

--- database.py
+++ database.py
@@ -1,9 +1,18 @@
 import mysql.connector
 
-def get_connection():
-    return mysql.connector.connect(
+try:
+    conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="quiz_app"
     )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1")
+    result = cursor.fetchone()
+
+    print("Database verbinding werkt! Resultaat:", result)
+
+except mysql.connector.Error as err:
+    print("Database fout:", err)
